24e18646e11fb11eac683cf2534e47e766d59d25
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-20 10:19:06 -0700

    getContext

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -13,6 +13,10 @@ class Diagram extends React.Component {
   }
 
   render() {
+    // need conditional logic in case browser doesn't support canvas
+    // this can be a conditional that tests for the .getContext('2D') method
+    // if (canvas.getContext)
+
     return <canvas id="diagram"></canvas>;
   }
 }
--- src/diagram-control/index.js
+++ src/diagram-control/index.js
@@ -1,4 +1,7 @@
 export default function diagramControl() {
-  const ctx = document.getElementById('diagram');
+  const canvas = document.getElementById('diagram');
+  const ctx = canvas.getContext('2d');
   console.log(ctx);
+  //   ctx.fillStyle = 'rgb(200, 0, 0)';
+  //   ctx.fillRect(10, 10, 50, 50);
 }
