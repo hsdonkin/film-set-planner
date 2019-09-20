f72edcbf53bed1d0a6eca963ff57edfb00b6f16a
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-20 15:39:35 -0700

    one rotatable asset with konva

--- src/components/Diagram.jsx
+++ src/components/Diagram.jsx
@@ -2,43 +2,39 @@ import React from 'react';
 import ReactKonva from 'react-konva';
 import { v4 } from 'uuid';
 import Arri_M8 from './../assets/Arri M8.png';
+import populateDiagram from '../diagram-control';
 
 class Diagram extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       objects: [
-        { name: 'Arri M8', id: v4(), imgPath: Arri_M8, x: 0, y: 0 },
-        { name: 'Arri M8', id: v4(), imgPath: Arri_M8, x: 50, y: 50 }
+        {
+          name: 'Arri M8',
+          id: v4(),
+          selected: false,
+          imgPath: Arri_M8,
+          x: 0,
+          y: 0
+        },
+        {
+          name: 'Arri M8',
+          id: v4(),
+          selected: true,
+          imgPath: Arri_M8,
+          x: 50,
+          y: 50
+        }
       ]
     };
   }
 
-  componentDidMount() {}
+  componentDidMount = () => {
+    populateDiagram(this.state.objects);
+  };
 
   render() {
-    const konvaLayers = this.state.objects.map(object => {
-      let img = new Image();
-      img.src = object.imgPath;
-      return (
-        <ReactKonva.Layer>
-          <ReactKonva.Image
-            image={img}
-            draggable={true}
-            x={object.x}
-            y={object.y}
-            key={object.id}
-          />
-        </ReactKonva.Layer>
-      );
-    });
-    console.log(konvaLayers);
-
-    return (
-      <ReactKonva.Stage height={600} width={800}>
-        {konvaLayers}
-      </ReactKonva.Stage>
-    );
+    return <div id="stage" />;
   }
 }
 
--- src/diagram-control/index.js
+++ src/diagram-control/index.js
@@ -1,9 +1,41 @@
-const Arri_M8_URL = require('./../assets/Arri M8.png');
+const ArriM8Url = require('./../assets/Arri M8.png');
+import Konva from 'konva';
 
-export default function diagramControl() {
-  const stage = new Konva.stage({
-    container: 'diagram',
-    width: 500,
-    height: 500
+export default function populateDiagram(objects) {
+  // takes in Diagram state as argument
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const stage = new Konva.Stage({
+    container: 'stage',
+    width: width,
+    height: height
   });
+
+  objects.forEach(object => {
+    let layer = new Konva.Layer();
+    stage.add(layer);
+
+    let imageObj = new Image();
+    let image = new Konva.Image({
+      x: object.x,
+      y: object.y,
+      image: imageObj,
+      draggable: true
+    });
+    imageObj.src = ArriM8Url;
+
+    layer.add(image);
+
+    if (object.selected === true) {
+      let tr1 = new Konva.Transformer({
+        node: image,
+        centeredScaling: true,
+        rotationSnaps: [0, 90, 180, 270],
+        resizeEnabled: false
+      });
+      layer.add(tr1);
+    }
+
+    layer.draw();
+  }); // close forEach
 }
