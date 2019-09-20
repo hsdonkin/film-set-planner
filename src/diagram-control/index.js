48f9de20971a64c4df815865d95d459558fa4874
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-09-20 11:51:58 -0700

    get image onto canvas via url = require './...path'

--- src/diagram-control/index.js
+++ src/diagram-control/index.js
@@ -1,7 +1,24 @@
+const Arri_M8_URL = require('./../assets/Arri M8.png');
+
 export default function diagramControl() {
+  // BIG difference here between CSS styles height / width of canvas, and canvas object height and width
+  // using CSS will stretch the canvas and mess with the coordinate system
+  // coordinates work like this:
+  //    0x
+  // 0y |------|
+  //    |------|
+  //    |------|
+  //  ctx.fillRect(x start, y start, width, height);
+
   const canvas = document.getElementById('diagram');
+  const DOMURL = window.URL || window.webkitURL || window;
+
+  canvas.setAttribute('height', 150);
+  canvas.setAttribute('width', 150);
   const ctx = canvas.getContext('2d');
-  console.log(ctx);
-  //   ctx.fillStyle = 'rgb(200, 0, 0)';
-  //   ctx.fillRect(10, 10, 50, 50);
+  const Arri_M8_IMG = new Image();
+  Arri_M8_IMG.src = Arri_M8_URL;
+  Arri_M8_IMG.onload = function() {
+    ctx.drawImage(Arri_M8_IMG, 0, 0);
+  };
 }
